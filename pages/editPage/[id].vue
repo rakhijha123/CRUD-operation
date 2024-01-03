4fbebc1b8138f6a958b1d333011be5ffdccd3ec6
rakhijha123 <rakhi.jha@hexafoldtech.com>
2024-01-03 17:50:08 +0530

    third-commite

--- pages/editPage/[id].vue
+++ pages/editPage/[id].vue
@@ -1,62 +1,48 @@
 <template>
   <div>
     <div class="header">
-        <h1>Employe</h1>
+        <h1 @click="goBackToHomePage">Employe</h1>
     </div>
     <div class="heading">
   <h1>edit details</h1>
 </div>
 <div class="container">
   <div class="left-box">
-<img :src="employees.avatar" alt="Avatar" />
-</div>
-{{ employees }}
+        <img :src="employees.avatar" alt="Avatar" />
+      </div>
 <div class="right-box">
- <h5>First-Name: <span><input v-model="employees.first_name"/></span></h5>
- <h5>Last-Name: <span>{{ employees.last_name}}</span></h5>
- <h5>Email: <span>{{ employees.email }}</span></h5>
+ <h5>First-Name: <span><input v-model="employees.data.first_name"/></span></h5>
+ <h5>Last-Name: <span>{{ employees.data.last_name}}</span></h5>
+ <h5>Email: <span>{{ employees.data.email }}</span></h5>
 </div>
 </div>
   </div>
 </template>
 
-
 <script setup>
- import { reactive, onMounted } from 'vue';
   import axios from 'axios';
   import { useRouter } from "vue-router";
-const router = useRouter();
+  const router = useRouter();
 
 
-  let employees = reactive({});
-// let id = ref("");
 
-let id = router.currentRoute.value.params.id;
+  const goBackToHomePage = () => {
+  router.push("/");
+  return;
+};
 
-  const fetchData1 = async () => {
+  let employees = reactive({
+    data: {}
+  });
+  const fetchDataById = async (id) => {
     try {
-      const response = await axios.get(`https://reqres.in/api/users/${id}`); 
-      console.log(response.data.data);
-
-      employees = response.data.data;
-      console.log(employees)
-
-
+      let response = await axios.get(`https://reqres.in/api/users/${id}`); 
+      employees.data = response.data.data;
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-
-  
-  
-  onMounted(async() => {
-
-     fetchData1();
-      // id.value = router.currentRoute.value.params.id;
-      console.log(router.currentRoute.value.params + "point1");
-   
-  });
-
-
-
+  onMounted(() => {
+    fetchDataById(router.currentRoute.value.params.id)
+  })
 </script>
